12dcf8258dcb7b12c8b0f1824bc943e78e277a94
amirshakabulov <kabulovamirsha@gmail.com>
2023-04-02 18:47:58 -0500

    added search result and buildgrid

--- routes/education.js
+++ routes/education.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Amirsha Kabulov' });
+  res.render('education', { title: 'Search Result for Education' });
 });
 
 module.exports = router;
